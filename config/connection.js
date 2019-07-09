963a52bdc8a3b97435a671bb39e27fbd51adcbe5
Victoire <victoirebaron@hotmail.fr>
2019-07-08 20:18:27 -0700

    connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 var mysql = require("mysql");
+var connection;
 
 if(process.env.JAWSDB_URL){
-  connetion = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
